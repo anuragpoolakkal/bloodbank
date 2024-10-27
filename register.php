e78973d1cf80ce4c3614c11d7a5c408da7f6bd3a
Anurag <anuragrajanp@gmail.com>
2024-10-27 11:41:01 +0530

    add styles.css

--- register.php
+++ register.php
@@ -40,6 +40,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <title>User Registration</title>
 </head>
 <body>
